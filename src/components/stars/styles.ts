d98347cfb168a7f32ac319f2c816821f70700b66
Mazal Mahari <who@Mazal-Mahari.local>
2024-12-05 18:00:27 +0200

    fix the start styles

--- src/components/stars/styles.ts
+++ src/components/stars/styles.ts
@@ -1,15 +1,13 @@
 import styled from "styled-components";
 import { ScreenBreakPoints } from "../../constants/BreakPoints";
 
-export const StarsContainer = styled.div `
-    display: none;
-    @media (min-width: ${ScreenBreakPoints.md}) {
-    display: flex;
-    align-items: center;
-    justify-content: center; 
-    height: 100%;
-    width: 100%;
-    
-  }
+export const StarsContainer = styled.div`
+  display: none; 
 
+ 
+    
+  @media (min-width: ${ScreenBreakPoints.md}px) { 
+    display:block; 
+
+    }
 `;
