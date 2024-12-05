65a1257287d478a618734a17d9b777ad90fe0956
Mazal Mahari <who@Mazal-Mahari.local>
2024-12-05 16:33:27 +0200

    price component

---
+++ src/components/Price/Price.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { PriceContainer, PriceText } from "./styles";
+
+interface PriceProps {
+  price: number;
+}
+
+const Price: React.FC<PriceProps> = ({ price }) => {
+  return (
+    <PriceContainer>
+      <PriceText>
+        ₪
+        {price}
+      </PriceText>
+    </PriceContainer>
+  );
+};
+
+export default Price;
