7284cd86626a1736ae0805a03874c509b476b009
Mazal Mahari <who@Mazal-Mahari.local>
2024-12-09 21:51:48 +0200

    improve the card styled

--- src/components/Card/styles.ts
+++ src/components/Card/styles.ts
@@ -1,20 +1,19 @@
 import styled from "styled-components";
+import { Colors } from "../colors";
 
 export const CardContainer = styled.div`
     display: flex;
     justify-content: center;
     align-items: center;
-    background-color: #f0f0f0;
-    max-height: 300px;
-    max-width: 300px;
+    background-color: ${Colors.Secondary.Beige};
+    width: 100%; 
+    height: auto;
+    padding: 16px; 
     border-radius: 10px;
     box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
     cursor: pointer;
-    
-    
-  &:hover {
-    transform: scale(1.05);
-    transition: transform 0.3s ease;
-  }
-  
+    &:hover {
+        transform: scale(1.05);
+        transition: transform 0.3s ease;
+    }
 `;
