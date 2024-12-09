d4cd1e9cf901f9dbdd77c4d3326b3e15c98bdf24
Mazal Mahari <who@Mazal-Mahari.local>
2024-12-09 21:53:31 +0200

    improve the styled and add optional props

--- src/components/IconMeaning/styles.ts
+++ src/components/IconMeaning/styles.ts
@@ -1,14 +1,19 @@
 import styled from "styled-components";
  import { ScreenBreakPoints } from "../../constants/BreakPoints";
 
+ 
+
+
 export const IconMeaningContainer = styled.img`
-        width: 30;
-        height: 23;
+
+    width: ${({ width }) => (width ? `${width}px` : '30px')}; 
+    height: ${({ height }) => (height ? `${height}px` : '23px')};  
     @media (min-width: ${ScreenBreakPoints.md}px){
-        width: 40;
-        height: 30;
+
+        width: ${({ width }) => (width ? `${width}px` : '40px')};  
+        height: ${({ height }) => (height ? `${height}px` : '31px')}; 
 
     }
-
   
 `;
+
