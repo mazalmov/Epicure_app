55f850853621649a1521e1c40e60f796709628d4
Mazal Mahari <who@Mazal-Mahari.local>
2024-12-10 11:42:05 +0200

    Deleting the components and moving them to the home page

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,9 @@
-import Header from "./components/Header/Header";
-import Hero from "./components/Hero/Hero";
-import PopularRestorants from "./components/PopularRestorants/PopularRestorants";
-import SignatureDish from "./components/SignatureDish/SignatureDish";
-import About from "./components/About/About";
-import TheMeaningOfOurIcons from "./components/TheMeaningOfOurIcons/TheMeaningOfOurIcons";
-import TopChef from "./components/TopChef/TopChef";
-import Footer from "./components/Footer/Footer";
+import HomePage from "./pages/HomePage";
 
 function App() {
   return (
-    <>
-      <Header />
-      <Hero />
-      <PopularRestorants />
-      <SignatureDish />
-      <TheMeaningOfOurIcons/>
-      <TopChef/>
-      <About/>
-      <Footer/>
-      
-
-
-    </>
+    <HomePage />
+    
   );
 }
 
