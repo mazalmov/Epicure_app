57fc646b5415511344cd59318c69c602274ac116
Mazal Mahari <who@Mazal-Mahari.local>
2024-12-10 09:56:43 +0200

    create footer

---
+++ src/components/Footer/styles.ts
@@ -0,0 +1,31 @@
+import styled from "styled-components";
+import { ScreenBreakPoints } from "../../constants/BreakPoints";
+import { Colors } from "../colors";
+import { Typography } from "../Typography";
+
+export const FooterContainer=styled.div`
+    display: flex;
+    flex-direction: column; 
+    align-items: center;    
+    padding: 30px;
+    width: 100%;
+    margin: 0 auto;
+    background: ${Colors.Secondary.White};
+    text-align: center; 
+    
+
+  @media (min-width: ${ScreenBreakPoints.md}px) {
+    flex-direction: row; 
+    justify-content: center;
+    align-items: center;  
+    gap: 30px;
+  }
+
+`;
+export const Text = styled.h2`
+  font-size: 36px;
+  margin-bottom: 20px;
+  color: #000;
+  height: 22px;
+  font: ${Typography.H2};
+`;
