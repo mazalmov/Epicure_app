bdfd30cea66c3e50a6fef2c1762ed95d9a4526c0
Mazal Mahari <who@Mazal-Mahari.local>
2024-12-13 05:20:39 +0200

    CardContainer styled

--- src/components/Card/styles.ts
+++ src/components/Card/styles.ts
@@ -1,16 +1,14 @@
 import styled from "styled-components";
 
 export const CardContainer = styled.div`
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    width: 100%; 
-    height: auto;
-    border-radius: 10px;
-    box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
-    cursor: pointer;
-    &:hover {
-        transform: scale(1.05);
-        transition: transform 0.3s ease;
-    }
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  align-items: center;
+  width: 100%;
+  height: 100%;
+  &:hover {
+    transform: scale(1.05);
+    transition: transform 0.3s ease;
+  }
 `;
