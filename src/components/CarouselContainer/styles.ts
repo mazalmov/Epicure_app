193ac823ee4eac38676a2ce746b5140d4dfc4ba5
Mazal Mahari <who@Mazal-Mahari.local>
2024-12-19 09:58:40 +0200

    Adding css styled to className: topChef

--- src/components/CarouselContainer/styles.ts
+++ src/components/CarouselContainer/styles.ts
@@ -2,7 +2,8 @@ import styled from 'styled-components';
 import { Typography } from '../Typography';
 import { ScreenBreakPoints } from '../../constants/BreakPoints';
 
-export const RestorantsContainer = styled.div`
+export const RestorantsContainer = styled.div<{ className?: string }>`
+  padding-left: 16px;
   margin: auto;
   align-items: center;
   justify-content: center;
@@ -14,14 +15,34 @@ export const RestorantsContainer = styled.div`
     flex-direction: row;
     padding-bottom: 80px;
     max-width: 1185px;
+    &.topChef {
+      padding-left: 0;
+    }
   }
+  @media (min-width: ${ScreenBreakPoints.mmd}px) {
+    padding-left: 16px;
+    &.topChef {
+      padding-left: 16px;
+    }
+  }
+  @media (min-width: ${ScreenBreakPoints.lg}px) {
+    padding-left: 0;
+    &.topChef {
+      padding-left: 0;
+      margin: auto;
+      align-items: left;
+      justify-content: flex-end;
+      width: 721px; 
+      margin: 0;
 
+    }
+  }
 `;
 
-export const Title = styled.h1`
+export const Title = styled.h1<{ className?: string }>`
   text-align: left;
   font-size: 36px;
-  margin-bottom: 20px;
+  margin-bottom: 40px;
   color: #000;
   ${Typography.H1};
   letter-spacing: 1.25px;
@@ -29,6 +50,11 @@ export const Title = styled.h1`
   @media (min-width: ${ScreenBreakPoints.md}px) {
     text-align: center;
     font-size: 30px;
+  }
+  @media (min-width: ${ScreenBreakPoints.lg}px) {
+    &.topChef {
+      text-align: left;
+    }
   }
 `;
 
